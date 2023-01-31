c6f13436377ae31041bff7c04e5b848ed3049fb7
Efe Furkan KARAKAYA <49307777+efefurkankarakaya@users.noreply.github.com>
2023-01-31 23:34:04 +0300

    test(app): tests are no longer co-dependent each other

--- test/fileOps_test.go
+++ test/fileOps_test.go
@@ -9,95 +9,6 @@ import (
 
 // $ go test -v ./test
 
-func TestCreatingTestingEnvironment(t *testing.T) {
-	// TODO: Refactor here.
-	const want bool = true
-
-	var err error
-	var message, destination string
-	var got bool
-
-	dir, err := os.Getwd()
-
-	if err != nil {
-		message = "Error occurred: os.Getwd()"
-		t.Errorf(message)
-	}
-
-	err = fileOperations.CreateTestingFolder(dir)
-
-	if err != nil {
-		println(err.Error())
-		message = "Error occurred: CreateTestingFolder()"
-		t.Errorf(message)
-	}
-
-	err = fileOperations.CopyTestingFilesToTestingFolder(dir)
-
-	if err != nil {
-		message = "Error occurred: CopyTestingFilesToTestingFolder"
-		t.Errorf(message)
-	}
-
-	destination = "temp"
-	got = fileOperations.CheckIfPathExists(destination)
-
-	if got != want {
-		message = "Failed. '" + destination + "' does not exist."
-		t.Errorf(message)
-	}
-
-	destination = "temp/lotofen.jpg"
-	got = fileOperations.CheckIfPathExists(destination)
-
-	if got != want {
-		message = "Failed. '" + destination + "' does not exist."
-		t.Errorf(message)
-	}
-}
-
-func TestFileOverwriting(t *testing.T) {
-	const want int64 = 0
-
-	var got int64
-	var message string
-	var size int64
-	var file os.FileInfo
-
-	const testDir string = "temp/"
-
-	filepath.Walk(testDir, func(path string, fileInfo os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-
-		file, err = os.Stat(path)
-		if err != nil {
-			return err
-		}
-
-		size = file.Size()
-
-		fileOperations.OverwriteFileZeroBytes(path)
-
-		file, err = os.Stat(path)
-		if err != nil {
-			return err
-		}
-
-		size = file.Size()
-
-		got = size
-
-		return err
-	})
-
-	if got != want {
-		message = "Overwrite test failed."
-		t.Errorf(message)
-	}
-}
-
 func TestFileDeletion(t *testing.T) {
 	const want bool = false
 	const testDir string = "temp/"
@@ -105,12 +16,15 @@ func TestFileDeletion(t *testing.T) {
 	var got bool
 	var message string
 
+	dir, _ := os.Getwd()
+	fileOperations.CreateTestingFolder(dir)
+	fileOperations.CopyTestingFilesToTestingFolder(dir)
+
 	filepath.Walk(testDir, func(path string, fileInfo os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
 
-		// TODO: Add remove directory here.
 		if !fileInfo.IsDir() {
 			fileOperations.RemoveFile(path)
 			got = fileOperations.CheckIfPathExists(path)
@@ -118,6 +32,8 @@ func TestFileDeletion(t *testing.T) {
 
 		return err
 	})
+
+	fileOperations.RemoveTree(testDir)
 
 	if got != want {
 		message = "File deletion test failed."
@@ -132,11 +48,11 @@ func TestTreeDeletion(t *testing.T) {
 	var got bool
 	var message string
 
-	err := fileOperations.RemoveTree(testDir)
-	if err != nil {
-		message = "Error occurred while removing tree."
-		t.Errorf(message)
-	}
+	dir, _ := os.Getwd()
+	fileOperations.CreateTestingFolder(dir)
+	fileOperations.CopyTestingFilesToTestingFolder(dir)
+
+	fileOperations.RemoveTree(testDir)
 
 	got = fileOperations.CheckIfPathExists(testDir)
 
